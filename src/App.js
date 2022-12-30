77a9a1009cde5dcef66187b224d12c600b54cc7b
Ashish Lavania <a1lavania@gmail.com>
2022-12-30 23:03:45 +0100

    Hello world

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div className="main__wrap">
+        <main className="container">
+          Hello World!
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
